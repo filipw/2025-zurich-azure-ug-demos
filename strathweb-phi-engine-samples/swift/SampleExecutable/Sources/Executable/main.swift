617506dede25ebbdc43637713a28ec4813c56749
filipw <filip@strathweb.com>
2025-01-15 19:07:11 +0100

    small clean up

--- strathweb-phi-engine-samples/swift/SampleExecutable/Sources/Executable/main.swift
+++ strathweb-phi-engine-samples/swift/SampleExecutable/Sources/Executable/main.swift
@@ -2,41 +2,17 @@ import Foundation
 
 let sourceFileDir = (#file as NSString).deletingLastPathComponent
 let basePath = ((sourceFileDir as NSString).appendingPathComponent("../../../../../fine-tuning/fused_model") as NSString).standardizingPath
-let modelProvider = PhiModelProvider.fileSystem(
-    indexPath: "\(basePath)/model.safetensors.index.json",
-    configPath: "\(basePath)/config.json"
-)
-
-let inferenceOptionsBuilder = InferenceOptionsBuilder()
-try! inferenceOptionsBuilder.withTemperature(temperature: 0.0)
-try inferenceOptionsBuilder.withTokenCount(contextWindow: 50)
-let inferenceOptions = try! inferenceOptionsBuilder.build()
-
-let cacheDir = (sourceFileDir as NSString).appending("/.cache")
-
-class ModelEventsHandler: PhiEventHandler {
-    func onInferenceStarted() {}
-
-    func onInferenceEnded() {}
-
-    func onInferenceToken(token: String) {}
-
-    func onModelLoaded() {
-        print("""
- 🧠 Model loaded!
-****************************************
-""")
-    }
-}
 
 let modelBuilder = PhiEngineBuilder()
-try! modelBuilder.withEventHandler(eventHandler: BoxedPhiEventHandler(handler: ModelEventsHandler()))
-let gpuEnabled = try! modelBuilder.tryUseGpu()
-try! modelBuilder.withModelProvider(modelProvider: modelProvider)
+_ = try modelBuilder.tryUseGpu()
+try modelBuilder.withModelProvider(modelProvider: PhiModelProvider.fileSystem(
+    indexPath: "\(basePath)/model.safetensors.index.json",
+    configPath: "\(basePath)/config.json"
+))
 
-let model = try! modelBuilder.build(cacheDir: cacheDir)
+let model = try modelBuilder.build(cacheDir: (sourceFileDir as NSString).appending("/.cache"))
+
 let context = ConversationContext(messages: [], systemInstruction: "")
-
 let prompts = [
     "Play alt rock",
     "too loud",
@@ -44,7 +20,7 @@ let prompts = [
     "Next one please",
     "Change song",
     "play Comfortably Numb",
-    "Go to last song",
+    "Go to lst song",
     "What's the time?",
     "pause it",
     "make it quieter",
@@ -55,7 +31,12 @@ let prompts = [
     "What's your favorite color?",
 ]
 
+let inferenceOptionsBuilder = InferenceOptionsBuilder()
+try inferenceOptionsBuilder.withTemperature(temperature: 0.0)
+try inferenceOptionsBuilder.withTokenCount(contextWindow: 50)
+let inferenceOptions = try inferenceOptionsBuilder.build()
+
 for prompt in prompts {
-    let result = try! model.runInference(promptText: prompt, conversationContext: context, inferenceOptions: inferenceOptions)
+    let result = try model.runInference(promptText: prompt, conversationContext: context, inferenceOptions: inferenceOptions)
     print("\(prompt)   ->   \(result.resultText)")
 }
